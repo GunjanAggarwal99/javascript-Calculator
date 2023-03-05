e3bf33e90265a050dacb71a4dc990f0d5107ef6b
GunjanAggarwal99 <106851409+GunjanAggarwal99@users.noreply.github.com>
2023-03-05 18:06:51 +0530

    using bind

--- index.js
+++ index.js
@@ -67,27 +67,27 @@ function calculationResult(operatorUSed) {
   writeToLog(currentResult, initialInput, getInputNumber, currentResult);
 }
 
-function add() {
-  calculationResult('ADD');
-  // const getInputNumber = getUserInput();
-  // const initialInput = currentResult;
-  // const descInput = `${currentResult} + ${getInputNumber}`;
-  // currentResult = currentResult + getInputNumber;
-  // outputResult(currentResult, descInput);
-  // createAndWriteOutput('+', initialInput, getInputNumber);
-  // writeToLog('ADD', initialInput, getInputNumber, currentResult);
-}
-function sub() {
-  calculationResult('SUB');
-}
-function mul() {
-  calculationResult('MUL');
-}
-function div() {
-  calculationResult('DIV');
-}
+// function add() {
+//   calculationResult('ADD');
+// const getInputNumber = getUserInput();
+// const initialInput = currentResult;
+// const descInput = `${currentResult} + ${getInputNumber}`;
+// currentResult = currentResult + getInputNumber;
+// outputResult(currentResult, descInput);
+// createAndWriteOutput('+', initialInput, getInputNumber);
+// writeToLog('ADD', initialInput, getInputNumber, currentResult);
+// }
+// function sub() {
+//   calculationResult('SUB');
+// }
+// function mul() {
+//   calculationResult('MUL');
+// }
+// function div() {
+//   calculationResult('DIV');
+// }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', sub);
-multiplyBtn.addEventListener('click', mul);
-divideBtn.addEventListener('click', div);
+addBtn.addEventListener('click', calculationResult.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculationResult.bind(this, 'SUB'));
+multiplyBtn.addEventListener('click', calculationResult.bind(this, 'MUL'));
+divideBtn.addEventListener('click', calculationResult.bind(this, 'DIV'));
